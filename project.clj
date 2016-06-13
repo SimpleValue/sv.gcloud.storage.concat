d13e6b6fc1c7f1642cc8042dfd4f00b2ed1dc7f6
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-13 13:08:49 +0200

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/gcloud.storage.concat "0.1.1"
+(defproject sv/gcloud.storage.concat "0.1.2-SNAPSHOT"
   :description "Concatenates files on Google Storage into one file."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
